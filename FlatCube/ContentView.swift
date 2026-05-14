6956c0202890b2359d92895486affda21d335c0e
Radomyr Sidenko <radik-sidehnko@ukr.net>
2026-05-14 17:37:04 +0300

    Initial Commit

---
+++ FlatCube/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlatCube
+//
+//  Created by Radomyr Sidenko on 14.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlatCube/FlatCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlatCubeApp.swift
+//  FlatCube
+//
+//  Created by Radomyr Sidenko on 14.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlatCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
